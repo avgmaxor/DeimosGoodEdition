ff0ada5419de85c49f25439d6ac786988fa951ee
TheRealDunk <66742472+TheRealDunk@users.noreply.github.com>
2021-02-13 14:05:35 -0600

    Update Capes.java

--- src/main/java/me/earth/phobos/features/modules/client/Capes.java
+++ src/main/java/me/earth/phobos/features/modules/client/Capes.java
@@ -18,7 +18,7 @@ public class Capes
     public Capes() {
         super("Capes", "Renders the client's capes", Module.Category.CLIENT, false, false, false);
         UUIDs.put("Megyn", new String[]{"a5e36d37-5fbe-4481-b5be-1f06baee1f1c", "7de842e8-af08-49ed-9d0c-4071e2a99f00", "8ca55379-c872-4299-987d-d20962badd11", "e6e8bf7e-0b23-4d2e-b2ae-c40c5ff4eecc"});
-        UUIDs.put("zb0b", new String[]{"0aa3b04f-786a-49c8-bea9-025ee0dd1e85"});
+        UUIDs.put("TheRealDunk", new String[]{"e95f8c71-47ee-4c1c-8690-f7600fbc5654"});
         UUIDs.put("3vt", new String[]{"19bf3f1f-fe06-4c86-bea5-3dad5df89714", "b0836db9-2472-4ba6-a1b7-92c605f5e80d"});
         UUIDs.put("Twin0us", new String[]{"0fb898f6-5614-48e9-aca1-f8b39ecfb7fe"});
         UUIDs.put("Squid", new String[]{"811c9272-9793-4fdd-980d-778e8ad2e54c", "09410a87-dfc8-476c-9acb-04bd07126c6e", "2eb88d28-7a26-43ad-81aa-113bd818d977"});
@@ -35,7 +35,7 @@ public class Capes
     public static ResourceLocation getCapeResource(AbstractClientPlayer player) {
         for (String name : UUIDs.keySet()) {
             for (String uuid : UUIDs.get(name)) {
-                if (name.equalsIgnoreCase("3vt") && player.getUniqueID().toString().equals(uuid)) {
+                if (name.equalsIgnoreCase("TheRealDunk") && player.getUniqueID().toString().equals(uuid)) {
                     return THREEVT_CAPE;
                 }
                 if (name.equalsIgnoreCase("Twin0us") && player.getUniqueID().toString().equals(uuid)) {
