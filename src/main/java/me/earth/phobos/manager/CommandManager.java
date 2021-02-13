72f4f755f16b5935b1b1b9287c02da09852d283d
TheRealDunk <66742472+TheRealDunk@users.noreply.github.com>
2021-02-13 09:30:18 -0600

    Update CommandManager.java

--- src/main/java/me/earth/phobos/manager/CommandManager.java
+++ src/main/java/me/earth/phobos/manager/CommandManager.java
@@ -9,7 +9,7 @@ import java.util.LinkedList;
 
 public class CommandManager
         extends Feature {
-    private String clientMessage = "<Phobos.eu>";
+    private String clientMessage = "[Phobos.cc]";
     private String prefix = ".";
     private final ArrayList<Command> commands = new ArrayList();
 
