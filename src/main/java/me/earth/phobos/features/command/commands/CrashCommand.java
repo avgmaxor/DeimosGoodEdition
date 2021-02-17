bd90624d93456d689ed2cda6ec5a4660843d8895
notperry1234567890 <65968863+notperry1234567890@users.noreply.github.com>
2021-02-16 17:23:19 -0800

    Update CrashCommand.java

--- src/main/java/me/earth/phobos/features/command/commands/CrashCommand.java
+++ src/main/java/me/earth/phobos/features/command/commands/CrashCommand.java
@@ -53,7 +53,7 @@ public class CrashCommand
                     list.appendTag(tString);
                 }
                 tag.setString("author", Util.mc.player.getName());
-                tag.setString("title", "phobos > all :^D");
+                tag.setString("title", "deimos > all :^D");
                 tag.setTag("pages", list);
                 bookObj.setTagInfo("pages", list);
                 bookObj.setTagCompound(tag);
